2f7419961cb69cf8a37f8d68a25073d01c1a5642
Lara Rodrigues <laravieirarodrigues@ua.pt>
2023-05-06 13:18:23 +0100

    updated build script

--- build.sh
+++ build.sh
@@ -1,27 +1,38 @@
 echo "Compiling source code."
-javac */*.java */*/*.java
+
+SRCFILES=src
+GENCLASS=/lib/genclass.jar
+CLASSPATH=$PWD$SRCFILES:$PWD$GENCLASS
+
+javac -cp $CLASSPATH src/*/*.java src/*/*/*.java
 
 echo "Distributing intermediate code to the different execution environments."
 
+if [ ! -d "temp" ]; then
+  mkdir temp
+fi
+
 echo "  AssaultParty"
-rm -rf dirAssaultParty
-mkdir -p dirAssaultParty dirAssaultParty/server dirAssaultParty/server/main dirAssaultParty/server/entities dirAssaultParty/server/sharedRegions \
-         dirAssaultParty/client dirAssaultParty/client/entities dirAssaultParty/client/stubs dirAssaultParty/utils
-cp server/main/Parameters.class server/main/ServerAssaultParty.class dirAssaultParty/server/main
-cp server/entities/AssaultPartyClientProxy.class dirAssaultParty/server/entities
-cp server/sharedRegions/AssaultPartyInterface.class server/sharedRegions/AssaultParty.class dirAssaultParty/server/sharedRegions
-cp client/entities/MasterThiefStates.class client/entities/OrdinaryThiefStates.class \
-   dirAssaultParty/client/entities
-cp utils/*.class dirAssaultParty/utils
+rm -rf temp/dirAssaultParty
+mkdir -p temp/dirAssaultParty temp/dirAssaultParty/server temp/dirAssaultParty/server/main temp/dirAssaultParty/server/entities temp/dirAssaultParty/server/sharedRegions \
+         temp/dirAssaultParty/client temp/dirAssaultParty/client/entities temp/dirAssaultParty/client/stubs temp/dirAssaultParty/utils
+cp src/utils/Parameters.class src/server/main/ServerAssaultParty.class temp/dirAssaultParty/server/main
+cp src/server/entities/AssaultPartyClientProxy.class temp/dirAssaultParty/server/entities
+cp src/server/sharedRegions/AssaultPartyInterface.class src/server/sharedRegions/AssaultParty.class temp/dirAssaultParty/server/sharedRegions
+cp src/client/entities/MasterThiefStates.class src/client/entities/OrdinaryThiefStates.class \
+   temp/dirAssaultParty/client/entities
+cp src/utils/*.class temp/dirAssaultParty/utils
 
 echo "Compressing execution environments."
 echo "  AssaultParty"
-rm -f  dirAssaultParty.zip
-zip -rq dirAssaultParty.zip dirBarberShop
+rm -f  temp/dirAssaultParty.zip
+cd temp
+zip -rq dirAssaultParty.zip dirAssaultParty
+cd ..
 
 echo "Deploying and decompressing execution environments."
 mkdir -p $PWD/test/Assault
 rm -rf $PWD/test/Assault/*
-cp dirAssaultParty.zip $PWD/test/Assault
+cp temp/dirAssaultParty.zip $PWD/test/Assault
 cd $PWD/test/Assault
 unzip -q dirAssaultParty.zip
