f001a89d29591c03faeb4c5f980b4d485bf96f16
Lara Rodrigues <laravieirarodrigues@ua.pt>
2023-05-08 00:56:46 +0100

    added fuser to kill ports

--- run_global.sh
+++ run_global.sh
@@ -12,10 +12,10 @@ MUSEUMHOST=sd$TAG@l040101-ws05.ua.pt
 MASTERTHIEFHOST=sd$TAG@l040101-ws06.ua.pt
 ORDINARYTHIEFHOST=sd$TAG@l040101-ws07.ua.pt
 
-xterm -T "AssaultPartyA" -hold -e "sshpass -f password ssh $ASSAULTPARTYAHOST './run_server.sh AssaultParty 22330 localhost 22331 /lib/genclass.jar'" &
-xterm -T "AssaultPartyB" -hold -e "sshpass -f password ssh $ASSAULTPARTYBHOST './run_server.sh AssaultParty 22332 localhost 22333 /lib/genclass.jar'" &
-xterm -T "CollectionSite" -hold -e "sshpass -f password ssh $COLLECTIONSITEHOST './run_server.sh CollectionSite 22334 localhost 22335 /lib/genclass.jar'" &
-xterm -T "ConcentrationSite" -hold -e "sshpass -f password ssh $CONCENTRATIONSITEHOST './run_server.sh ConcentrationSite 22336 localhost 22337 /lib/genclass.jar'" &
-xterm -T "Museum" -hold -e "sshpass -f password ssh $MUSEUMHOST './run_server.sh Museum 22338 localhost 22339 /lib/genclass.jar'" &
+xterm -T "AssaultPartyA" -hold -e "sshpass -f password ssh $ASSAULTPARTYAHOST 'fuser -k 22330/tcp; ./run_server.sh AssaultParty 22330 localhost 22331 /lib/genclass.jar'" &
+xterm -T "AssaultPartyB" -hold -e "sshpass -f password ssh $ASSAULTPARTYBHOST 'fuser -k 22332/tcp; ./run_server.sh AssaultParty 22332 localhost 22333 /lib/genclass.jar'" &
+xterm -T "CollectionSite" -hold -e "sshpass -f password ssh $COLLECTIONSITEHOST 'fuser -k 22334/tcp; ./run_server.sh CollectionSite 22334 localhost 22335 /lib/genclass.jar'" &
+xterm -T "ConcentrationSite" -hold -e "sshpass -f password ssh $CONCENTRATIONSITEHOST 'fuser -k 22336/tcp; ./run_server.sh ConcentrationSite 22336 localhost 22337 /lib/genclass.jar'" &
+xterm -T "Museum" -hold -e "sshpass -f password ssh $MUSEUMHOST 'fuser -k 22338/tcp; ./run_server.sh Museum 22338 localhost 22339 /lib/genclass.jar'" &
 xterm -T "MasterThief" -hold -e "sshpass -f password ssh $MASTERTHIEFHOST './run_client.sh MasterThief /lib/genclass.jar'" &
 xterm -T "OrdinaryThief" -hold -e "sshpass -f password ssh $ORDINARYTHIEFHOST './run_client.sh OrdinaryThief /lib/genclass.jar'"
