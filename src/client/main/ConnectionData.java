c3f14e0e680463b7c2c8af6058a0b35662ef36c4
Diogo Correia <diogo.correia99@ua.pt>
2023-05-08 00:57:19 +0100

    set machine names

--- src/client/main/ConnectionData.java
+++ src/client/main/ConnectionData.java
@@ -1,17 +1,17 @@
 package client.main;
 
 public class ConnectionData {
-    public static final String ASSAULT_PARTY_0_MACHINE = "localhost";
+    public static final String ASSAULT_PARTY_0_MACHINE = "l040101-ws01.ua.pt";
     public static final int ASSAULT_PARTY_0_PORT = 22330;
-    public static final String ASSAULT_PARTY_1_MACHINE = "localhost";
+    public static final String ASSAULT_PARTY_1_MACHINE = "l040101-ws02.ua.pt";
     public static final int ASSAULT_PARTY_1_PORT = 22332;
 
-    public static final String COLLECTION_SITE_MACHINE = "localhost";
+    public static final String COLLECTION_SITE_MACHINE = "l040101-ws03.ua.pt";
     public static final int COLLECTION_SITE_PORT = 22334;
 
-    public static final String CONCENTRATION_SITE_MACHINE = "localhost";
+    public static final String CONCENTRATION_SITE_MACHINE = "l040101-ws04.ua.pt";
     public static final int CONCENTRATION_SITE_PORT = 22336;
 
-    public static final String MUSEUM_MACHINE = "localhost";
+    public static final String MUSEUM_MACHINE = "l040101-ws05.ua.pt";
     public static final int MUSEUM_PORT = 22338;
 }
