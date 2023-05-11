c7cb8c3479974a969eeabc55e3b4226023f1f04b
Lara Rodrigues <laravieirarodrigues@ua.pt>
2023-05-11 17:40:33 +0100

    added sleep to run

--- run_global.sh
+++ run_global.sh
@@ -47,6 +47,7 @@ echo -e "\nRunning servers on machines, check the terminals that pop up on your 
 
 if [ $# -eq 0 ]; then
   run_servers &
+  sleep 1
   run_clients
 else
   if [ $1 = "servers" ]; then
